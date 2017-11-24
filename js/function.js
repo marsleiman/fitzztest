54d7c969fbaec651f877caf9719734297314fe86
Mariam Sleiman <msleiman@grupovida.com>
2017-11-24 18:31:08 -0300

    functions

--- js/function.js
+++ js/function.js
@@ -1,1 +1,12 @@
-$('.dropdown-toggle').dropdown()
+$(document).ready(function(){
+    $("p.price").hover(function(){
+        $(this).addClass("green");
+        }, function(){
+        $(this).removeClass("green");
+    });
+    $("figcaption.title-product").hover(function(){
+        $(this).addClass("title-product2");
+        }, function(){
+        $(this).removeClass("title-product2");
+    });
+});
